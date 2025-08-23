5525ce3b54f96e40765ae7189d45dc9a93c067ff
buja23 <jooj.soares.227@gmail.com>
2025-08-23 01:47:26 -0300

    "react inicio, socket e servicess"

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const http = require("http")
 const {serve} = require("socket.io")
 const cors = require("cors")
+const { Socket } = require("socket.io-client")
 
 const app = express()
 app.use(cors())
@@ -14,6 +15,35 @@ const io = new Server(server,{
     }
 })
 
+// Array com as salas disponíveis
 const rooms = ["filmes", "jogos", "musicas"]
 
+io.on("connection", (socket) => {
+    console.log(`novo usuario conectado, ${socket.id}`)
 
+    // Usuário pede para entrar em uma sala
+    socket.on("joinRoom", (roomName) => {
+        if (rooms.includes(roomName)) {
+            socket.join(roomName)
+            console.log(`Usuario ${socket.id} entrou na sala ${roomName}`)
+
+            // avisa pra sala que entrou um novo usuário
+            io.to(roomName).emit("receiveMessage", {
+                user: "Sistema",
+                message: `Novo usuario entrou na sala ${roomName}`
+            })
+        }
+    })
+
+    socket.on("sendMessage", (data) => {
+        io.to(data.room).emit("receiveMessage", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log(`Usuario desconectado:${socket.id}`)
+    })
+
+    server.listen(3001, () =>{
+        console.log("Servidor rodando na porta 3001")
+    })
+})
